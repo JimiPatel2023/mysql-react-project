cd5c8ca102a7cd4c777da85c04694662e449c0b9
Jimi Patel - Github <pateljimi2020@gmail.com>
2023-10-19 21:58:41 +0530

    safe4

--- backend/utils/saveCookie.js
+++ backend/utils/saveCookie.js
@@ -2,6 +2,8 @@ const saveCookie = (token, res) => {
   res.cookie("token", token, {
     expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    secure: true,
+    domain: "td-frontend-beta.vercel.app",
   });
 };
 
