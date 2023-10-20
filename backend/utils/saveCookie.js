4a14bff762e1058087d3f111f620ff75148b9ccf
Jimi Patel - Github <pateljimi2020@gmail.com>
2023-10-20 14:03:55 +0530

    safw12

--- backend/utils/saveCookie.js
+++ backend/utils/saveCookie.js
@@ -1,8 +1,7 @@
 const saveCookie = (token, res, days = 5) => {
   res.cookie("token", token, {
-    expires: new Date(Date.now() + days * 24 * 60 * 60 * 1000),
     httpOnly: false,
-    secure: true,
+    expires: new Date(Date.now() + days * 24 * 60 * 60 * 1000),
     sameSite: "none",
   });
 };
